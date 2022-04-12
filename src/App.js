4abe302d218fed1084b01d33591b0be096910ebc
Coffee Coder Sa <bianca.kleynhans93@gmail.com>
2022-04-12 08:05:46 +0200

    Add files via upload
    Intro redact with redux as well as sample code and sumary notes covering the basics of react and reux

--- src/App.js
+++ src/App.js
@@ -1,65 +1,35 @@
-import "./App.css";
-
-let name = "any tex with qua"; // string
-let age = 10; //number
-let arr = ["string array"]; //array
-let bool = true; // boolean
-let obj = { key: "value" }; // object
-
-function Greet(name, age) {
-  console.log(`Good day ${name}, you are ${age} years old`);
-  console.log("Good day" + name + ", you are" + age);
-  return <p>{`Good day ${name}, you are ${age} years old`}</p>;
-}
-
-// Create function named addToghter takes 2 params of type number add the two params togther and console logs it the total to the screen
-function addTogether(firstval, secondval) {
-  console.log(`first value is ${firstval}, second is ${secondval} `);
-  let total = firstval + secondval;
-  console.log(total);
-  return <p>{`${firstval} + ${secondval} = ${total}`}</p>;
-}
-
-// Practical Self Study answers
-function Q1() {
-  const name = "bianca";
-  const age = 28;
-  const dob = "9 July 1993";
-  const fave = "purple";
-  return <p>{`My name is ${name} I am ${age} years old. I was born on ${dob}. My fav color is ${fave}`}</p>;
-}
-
-function Q2(val1, val2) {
-  let arr = [];
-  for (let i = 0; i < val1; i++) {
-    arr.push(val2);
-  }
-  return arr;
-}
-
-function Q4() {
-  return <h1>DONE</h1>;
-}
+import React from "react";
+import { Counter } from "./components/Counter";
+import logo from "./assets/logo.svg";
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello world</h1>
-      {Greet(name, age)}
-      {/*  Display the value of addToghter under a heading */}
-      {addTogether(100, 2.5)}
-
-      {/* Q3 */}
-      <h2>Meet me </h2>
-      {Q1()}
-
-      <br />
-
-      <ul>
-        {Q2(10, "new Text").map((entry) => {
-          return <li>{entry}</li>;
-        })}
-      </ul>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <Counter />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a className='App-link' href='https://reactjs.org/' target='_blank' rel='noopener noreferrer'>
+            React
+          </a>
+          <span>, </span>
+          <a className='App-link' href='https://redux.js.org/' target='_blank' rel='noopener noreferrer'>
+            Redux
+          </a>
+          <span>, </span>
+          <a className='App-link' href='https://redux-toolkit.js.org/' target='_blank' rel='noopener noreferrer'>
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a className='App-link' href='https://react-redux.js.org/' target='_blank' rel='noopener noreferrer'>
+            React Redux
+          </a>
+        </span>
+      </header>
     </div>
   );
 }
